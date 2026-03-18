4428b5358f3c32665807444b1c6f87051a13d909
nakeshinex-ai <nakeshinex@gmail.com>
2026-03-18 14:17:26 +0000

    Se creo el programa para calculadora

---
+++ CALCULADORA/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    float f, g, h;
+    char op, op2;
+
+    printf("Selecciona una opcion\n");
+    printf("1. Operaciones con enteros\n");
+    printf("2. Operaciones con reales\n");
+
+    scanf("%c" , &op);
+
+    printf("Selecciona una opcion\n");
+    printf("1. Suma\n");
+    printf("2. Resta\n");
+    printf("3. Multriplicacion\n");
+    printf("4. Division\n");
+    printf("5. Modulo\n");
+    printf("6. Salir")
+    scanf("%c" , &op2);
+
+        if (op == '1')
+    {
+        printf("Ingresa los dos numeros con los q quieres operar(num1 num2\n)");
+        scanf("%d %d", &a, &b); 
+        switch (op2)
+        {
+        case 1:
+            c = a + b;
+            break;
+        case 2:
+            c = a - b;
+            break;
+        case 3:
+            c = a * b;
+            break;
+        case 4:
+            c = a / b 
+            break;
+        case 5:
+            c = a % b 
+            break;
+
+        default:
+            break;
+        }
+        default:
+            return 0
+
+        }
+printf("El resultado es %d\n", c)
+    }else{ 
+        printf("Ingresa los dos numeros con los q quieres operar(num1 num2\n)");
+        scanf("%d %d", &a &b); 
+}
